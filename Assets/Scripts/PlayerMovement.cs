1461523223fb710b1812acbb2a6f94ef4d664d04
DennisLi755 <70350348+DennisLi755@users.noreply.github.com>
2022-08-24 23:53:52 -0400

    dash physics/effect, inertia camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 7f;
 
     public Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
 
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    private float dashLength = 0.15f;
+    private float dashCooldown = 0.2f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    public GameObject echo;
+    private float echoSpawns;
+    private bool isDashing;
+    void Start() {
+        dashSpeed = 30f;
+        echoSpawns = 0.01f;
+        activeMoveSpeed = moveSpeed;
+        isDashing = false;
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.X)) {
+            if (dashCoolCounter <= 0 && dashCounter <= 0) {
+                isDashing = true;
+                Instantiate(echo, transform.position, Quaternion.identity);
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0) {
+            dashCounter -= Time.deltaTime;
+            if (dashCounter <= 0) { 
+                isDashing = false;
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0) {
+            dashCoolCounter -= Time.deltaTime;
+        }
 
         if (movement != Vector2.zero) {
             animator.SetFloat("Horizontal", movement.x);
@@ -27,6 +69,16 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * activeMoveSpeed * Time.fixedDeltaTime);
+        if (isDashing) {
+            if (echoSpawns <= 0) {
+                GameObject echoInstance = Instantiate(echo, transform.position, Quaternion.identity);
+                echoInstance.GetComponent<SpriteRenderer>().sprite = this.GetComponent<SpriteRenderer>().sprite;
+                Destroy(echoInstance, 1f);
+                echoSpawns = 0.01f;
+            } else {
+                echoSpawns -= Time.deltaTime*2;
+            }
+        }
     }
 }
