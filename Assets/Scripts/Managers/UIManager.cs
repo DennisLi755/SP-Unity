2e039e0752df1377b9d4e2e6251c5b233e72e996
meideka20 <kfm5870@rit.edu>
2023-02-21 18:10:27 -0500

    health bar exists

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -19,6 +19,8 @@ public class UIManager : MonoBehaviour {
     [SerializeField]
     EventSystem eventSystem;
     [SerializeField]
+    Slider health;
+    [SerializeField]
     private GameObject prompt;
     public GameObject Prompt {get => prompt; set {prompt = value;}}
     #endregion
