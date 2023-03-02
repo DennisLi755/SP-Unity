2c09b2c7db4404ef2d8291cd7f2751469764204d
DennisLi755 <djl6347@g.rit.edu>
2023-03-02 12:49:24 -0500

    wireframed load file menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject titleMenu;
+    [SerializeField]
+    private GameObject loadMenu;
+    [SerializeField]
+    private Button newGameButton;
+    [SerializeField]
+    private Button fileOneButton;
+    [SerializeField]
+    private EventSystem es;
     public void NewGame() {
         SceneManager.LoadSceneAsync("Enemy Testing");
     }
 
-    public void LoadGame() {
+    public void OpenLoadMenu() {
+        titleMenu.SetActive(false);
+        loadMenu.SetActive(true);
+        es.SetSelectedGameObject(fileOneButton.gameObject);
+    }
 
+    public void OpenTitleMenu() {
+        titleMenu.SetActive(true);
+        loadMenu.SetActive(false);
+        es.SetSelectedGameObject(newGameButton.gameObject);
+    }
+
+    public void LoadSaveFile(int index) {
+        
     }
 
     public void OptionsMenu() {
