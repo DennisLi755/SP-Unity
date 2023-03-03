b30c0b1de8a2dad6be5f1fa76789a744132c1513
cajunwildcat <elicheramie89@gmail.com>
2023-03-03 18:12:07 -0500

    added boss fight room to Somni House scene

--- Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
+++ Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class GuideInteract : InteractableObject {
-    [SerializeField]
-    private YarnProject project;
-    [SerializeField]
-    private string startNode;
-    public override void OnInteract() {
-        DialogueManager.Instance.StartDialogue(project, startNode);
+public class GuideInteract : PromptedInteraction {
+    List<string> beforeText = new List<string>() { "Go pick up your sword, then we can fight." };
 
-        base.OnInteract();
+    List<string> afterText = new List<string>() { "Do you want to fight? " };
+
+    public override void OnInteract() {
+        if (PlayerInfo.Instance.AttackUnlocked) {
+            UIManager.Instance.ActivatePromptedInteraction(this, afterText);
+        }
+        else {
+            UIManager.Instance.ActivateInteractText(beforeText);
+        }
     }
 }
