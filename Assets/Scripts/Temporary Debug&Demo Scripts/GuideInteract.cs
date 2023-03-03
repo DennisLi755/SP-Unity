02442f1998c38d22ddcd69b6eb0f4d0289011507
cajunwildcat <elicheramie89@gmail.com>
2023-03-03 18:23:05 -0500

    started R* demo scene

--- Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
+++ Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
@@ -8,6 +8,9 @@ public class GuideInteract : PromptedInteraction {
 
     List<string> afterText = new List<string>() { "Do you want to fight? " };
 
+    public GameObject GuideBoss;
+    public GameObject BossRoom;
+
     public override void OnInteract() {
         if (PlayerInfo.Instance.AttackUnlocked) {
             UIManager.Instance.ActivatePromptedInteraction(this, afterText);
@@ -15,5 +18,25 @@ public class GuideInteract : PromptedInteraction {
         else {
             UIManager.Instance.ActivateInteractText(beforeText);
         }
+    }
+
+    public override void OnYes() {
+        StartCoroutine(BossStart());
+
+        base.OnYes();
+    }
+
+    IEnumerator BossStart() {
+        UIManager.Instance.FadeToBlack();
+        yield return new WaitForSeconds(0.5f);
+        BossRoom.GetComponent<Room>().MoveCameraHere();
+        PlayerInfo.Instance.transform.position = new Vector3(97.0f, -80.75f, 0.0f);
+
+        UIManager.Instance.FadeFromBlack();
+        yield return new WaitForSeconds(0.5f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        Instantiate(GuideBoss, new Vector3(96.4f, -77.33f, 0.0f), Quaternion.identity);
     }
 }
