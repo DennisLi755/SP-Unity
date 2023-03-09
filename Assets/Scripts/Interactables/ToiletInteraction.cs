bf29915fd598d74382cd969279d4649f32572223
DennisLi755 <djl6347@g.rit.edu>
2023-03-09 18:59:42 -0500

    started toilet interaction

---
+++ Assets/Scripts/Interactables/ToiletInteraction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletInteraction : InteractableObject
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
