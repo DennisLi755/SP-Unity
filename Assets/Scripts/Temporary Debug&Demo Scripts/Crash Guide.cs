58ed32bc2cb379ed7b15040bb5042a3103d46239
cajunwildcat <elicheramie89@gmail.com>
2023-03-24 18:19:51 -0400

    started guide interaction script

---
+++ Assets/Scripts/Temporary Debug&Demo Scripts/Crash Guide.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class CrashGuide : InteractableObject {
+    private Animator anim;
+    [SerializeField]
+    YarnProject dialogue;
+
+    public void Start() {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    public override void OnInteract() {
+        StartCoroutine(StandUp());
+    }
+
+    private IEnumerator StandUp() {
+        anim.SetTrigger("Stand Up");
+        PlayerInfo.Instance.PlayerControl.Freeze();
+        yield return new WaitForSeconds(2.0f);
+        //start dialogue
+        DialogueManager.Instance.StartDialogue(dialogue, "Meet_Guide");
+    }
+}
