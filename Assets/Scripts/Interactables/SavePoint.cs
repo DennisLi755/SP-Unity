0abaae7fd35370d3353075d16b03df02d3693ca7
cajunwildcat <elicheramie89@gmail.com>
2023-03-24 20:37:03 -0400

    boss trigger

--- Assets/Scripts/Interactables/SavePoint.cs
+++ Assets/Scripts/Interactables/SavePoint.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class SavePoint : InteractableObject {
     [SerializeField]
     private Vector3 playerPosition;
-    public Vector3 PlayerPosition;
+    public Vector3 PlayerPosition => playerPosition;
 
+#if UNITY_EDITOR
     private void OnDrawGizmos() {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireCube(PlayerPosition + transform.position, new Vector3(0.491736591f, 0.384848833f, 0.0f));
     }
+#endif
 
     public override void OnInteract() {
         GameManager.Instance.SavePlayerData(gameObject.name);
+        PlayerInfo.Instance.EnableTutorialText("Saved!");
+    }
+
+    IEnumerator TurnOffSaveText() {
+        yield return new WaitForSeconds(1.5f);
+        PlayerInfo.Instance.DisableTutorialText();
     }
 }
