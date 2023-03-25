dab1433b5cc4e0197875d3faa2348f0794f0710b
DennisLi755 <djl6347@g.rit.edu>
2023-03-24 20:16:09 -0400

    added guide interact dialogue

--- Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
+++ Assets/Scripts/Temporary Debug&Demo Scripts/GuideInteract.cs
@@ -3,46 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class GuideInteract : PromptedInteraction {
-    List<string> beforeText = new List<string>() { "Go pick up your sword, then we can fight." };
-
-    List<string> afterText = new List<string>() { "Do you want to fight? " };
-
-    public GameObject GuideBoss;
-    public GameObject BossRoom;
-
-    private bool settingUpFight = false;
-
+public class GuideInteract : InteractableObject {
     public override void OnInteract() {
-        if (!settingUpFight) {
-            if (PlayerInfo.Instance.AttackUnlocked) {
-                UIManager.Instance.ActivatePromptedInteraction(this, afterText);
-            }
-            else {
-                UIManager.Instance.ActivateInteractText(beforeText);
-            }
-        }
-    }
-
-    public override void OnYes() {
-        StartCoroutine(BossStart());
-
-        base.OnYes();
-    }
-
-    IEnumerator BossStart() {
-        settingUpFight = true;
-        UIManager.Instance.FadeToBlack();
-        yield return new WaitForSeconds(0.5f);
-        BossRoom.GetComponent<Room>().MoveCameraHere();
-        PlayerInfo.Instance.transform.position = new Vector3(97.0f, -80.75f, 0.0f);
-
-        UIManager.Instance.FadeFromBlack();
-        yield return new WaitForSeconds(0.5f);
-
-        yield return new WaitForSeconds(0.5f);
-
-        Instantiate(GuideBoss, new Vector3(96.4f, -77.33f, 0.0f), Quaternion.identity);
-        settingUpFight = false;
+        DialogueManager.Instance.StartDialogue("Guide_Interact");
     }
 }
