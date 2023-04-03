f4c253e2de6ecf8d440e4c24107f732f26af6c6d
cajunwildcat <elicheramie89@gmail.com>
2023-04-03 18:45:34 -0400

    added BulletHolder.ClearBullets

--- Assets/Scripts/Managers/BulletHolder.cs
+++ Assets/Scripts/Managers/BulletHolder.cs
@@ -18,10 +18,17 @@ public class BulletHolder : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Whether there are any bullets in the scene currently
+    /// </summary>
+    /// <returns></returns>
     public static bool HasChildren() {
         return instance.transform.childCount > 0;
     }
 
+    /// <summary>
+    /// Adds a bullet to the bullet count, if the count was 0 before enters the player into combat
+    /// </summary>
     public void AddBullet() {
         if (bulletCount == 0) {
             PlayerInfo.Instance.EnterCombat();
@@ -29,10 +36,24 @@ public class BulletHolder : MonoBehaviour {
         bulletCount++;
     }
 
+    /// <summary>
+    /// Reduces the bullet count, if 0 afterwards removes the player from combat
+    /// </summary>
     public void RemoveBullet() {
         bulletCount--;
         if (bulletCount == 0) {
             PlayerInfo.Instance.ExitCombat();
+        }
+    }
+
+    /// <summary>
+    /// Removes all bullets in the scene
+    /// </summary>
+    [ContextMenu("Cull Bullets")]
+    public void ClearBullets() {
+        Bullet[] bullets = FindObjectsOfType<Bullet>();
+        foreach (Bullet b in bullets) {
+            Destroy(b.gameObject);
         }
     }
 }
